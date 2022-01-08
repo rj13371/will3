4a3c3dee50222664399e3942082982ce70621218
junoware <bryan@junoware.com>
2022-01-07 18:35:24 -0800

    changed header component to link to home page

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+    <a href="./" rel="noopener noreferrer">
       <PageHeader title="🪦 Will3" subTitle="Your last wishes, decentralized." style={{ cursor: "pointer" }} />
     </a>
   );
