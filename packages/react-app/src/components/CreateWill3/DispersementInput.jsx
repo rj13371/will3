53d72660f4bd2f146e6e63957ab0890d3a02026f
junoware <bryan@junoware.com>
2022-01-09 10:49:16 -0800

    changed button copy

--- packages/react-app/src/components/CreateWill3/DispersementInput.jsx
+++ packages/react-app/src/components/CreateWill3/DispersementInput.jsx
@@ -64,7 +64,7 @@ export default function DispersementInput(tokenAddress) {
       </Form.List>
       <Form.Item>
         <Button type="primary" htmlType="submit" onClick={() => console.log("do trigger function")}>
-          Submit Transaction
+          Create Will3
         </Button>
       </Form.Item>
     </Form>
