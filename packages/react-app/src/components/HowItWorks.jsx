1e4da9fd916a8c601b2b78e58338eaac6ca997ea
junoware <bryan@junoware.com>
2022-01-09 18:05:16 -0800

    copy updates and filling in HowItWorks section

--- packages/react-app/src/components/HowItWorks.jsx
+++ packages/react-app/src/components/HowItWorks.jsx
@@ -9,21 +9,37 @@ function callback(key) {
   console.log(key);
 }
 
-const text = `We ask that you please provide percentages and wallet addresses for your benefactor allotment of your assets.`;
-
 export default function HowItWorks() {
   return (
     <div style={{ margin: "80px 0px" }}>
       <h2>How It Works</h2>
       <Collapse style={{ textAlign: "left" }} defaultActiveKey={["1"]} onChange={callback}>
-        <Panel header="This is panel header 1" key="1">
-          <p>{text}</p>
+        <Panel header="What is Will3?" key="1">
+          <p>
+            Will3 acts as a trustless executor sending predetermined assets to your designated benefactors upon your
+            final life event.
+          </p>
         </Panel>
-        <Panel header="This is panel header 2" key="2">
-          <p>{text}</p>
+        <Panel header="How do I use Will3?" key="2">
+          <p>
+            Connect your metamask and designate percentages of assets to your benefactories. In the event of your death,
+            they will be dispersed accordingly. That's it. Easy!
+          </p>
         </Panel>
-        <Panel header="This is panel header 3" key="3">
-          <p>{text}</p>
+        <Panel header="Why should I have a Will3?" key="3">
+          <p>text</p>
+        </Panel>
+        <Panel header="When should I update my Will3?" key="4">
+          <p>text</p>
+        </Panel>
+        <Panel header="What happens to the money in my wallet when I am not using it?" key="5">
+          <p>text</p>
+        </Panel>
+        <Panel header="How should I prepare my wallet for Will3?" key="6">
+          <p>text</p>
+        </Panel>
+        <Panel header="Can I designate money to charity in my Will3?" key="7">
+          <p>text</p>
         </Panel>
       </Collapse>
     </div>
--- packages/react-app/src/views/UI.jsx
+++ packages/react-app/src/views/UI.jsx
@@ -24,26 +24,25 @@ export default function UI({
       <div style={{ padding: 16, width: "80%", margin: "auto", marginTop: 24, paddingBottom: 160 }}>
         <img src={titleImage} style={{ width: "25%" }} />
         <h2 style={{ marginTop: 24 }}>Blockchain is predictable. Life is not.</h2>
-        <h5 style={{ marginTop: 48 }}>
-          Keep your loved ones and assets safe in your death. Allocate your assets safely and in a trustless fashion.
-          Using the Will3 protocol, Will3 will monitor your life status and in the event of your death, confirm and
-          honor your last wishes. Decentralized death is a trustless way to make sure your crypto assets end up in the
-          right hands.
+        <h5 style={{ width: "800px", margin: "auto", marginTop: 24, fontWeight: "normal" }}>
+          Allocate your assets safely and in a trustless fashion. Will3 monitors your life status and in the event of
+          your death, confirms and honors your last wishes. Decentralize death and make sure your crypto assets end up
+          in the right hands.
         </h5>
-        <div style={{ margin: 8, marginBottom: 80 }}>
+        <div style={{ margin: 8, marginBottom: 80, marginTop: 24 }}>
           {!address ? (
             <Button className="connect-wallet-button" onClick={loadWeb3Modal}>
-              Connect your wallet to continue
+              <h5 style={{ color: "black" }}>Connect your wallet to continue</h5>
             </Button>
           ) : (
-            <Button className="connect-wallet-button">
+            <Button style={{ height: "auto" }} className="connect-wallet-button">
               <Link
                 onClick={() => {
                   setRoute("/will3");
                 }}
                 to="/will3"
               >
-                Enter App
+                <h5 style={{ color: "black", margin: "auto" }}>Enter App</h5>
               </Link>
             </Button>
           )}
