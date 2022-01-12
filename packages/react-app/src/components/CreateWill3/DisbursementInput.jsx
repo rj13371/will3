8965e5d1b8cad796827f773f6bbc413904d58e97
Roland M <rj1337@gmail.com>
2022-01-12 13:50:23 +0900

    make email input bigger

--- packages/react-app/src/components/CreateWill3/DisbursementInput.jsx
+++ packages/react-app/src/components/CreateWill3/DisbursementInput.jsx
@@ -147,7 +147,7 @@ export default function DisbursementInput({ tx, writeContracts, userAddress }) {
         )}
       </Form.List>
 
-      <Form.Item label="Subscribe Email (optional)" name="email" style={{ maxWidth: 300 }}>
+      <Form.Item label="Subscribe Email (optional)" name="email" style={{ maxWidth: 450 }}>
         <Input />
       </Form.Item>
 
