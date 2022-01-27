27320a66f7228f39a3dd7184fdbf435cf4bea2dc
Roland M <rj1337@gmail.com>
2022-01-27 11:30:01 +0900

    add logo and favicon

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -3,14 +3,12 @@ import React from "react";
 
 // displays a page header
 
+const logo = "assets/will3logo.png";
+
 export default function Header() {
   return (
     <a href="./" rel="noopener noreferrer">
-      <PageHeader
-        avatar={{ src: "https://avatars1.githubusercontent.com/u/8186664?s=460&v=4" }}
-        subTitle="Your last wishes, decentralized."
-        style={{ cursor: "pointer" }}
-      />
+      <PageHeader avatar={{ src: logo }} subTitle="Your last wishes, decentralized." style={{ cursor: "pointer" }} />
     </a>
   );
 }
