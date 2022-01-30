0dd936ebb6c258d746edb488d37eeb06702c0306
junoware <bryan@junoware.com>
2022-01-29 23:45:10 -0800

    clean up

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -388,7 +388,6 @@ function App(props) {
 
   return (
     <div className="App">
-      {/* ✏️ Edit the header and change the title to your project name */}
       <Header />
       {networkDisplay}
       <BrowserRouter>
